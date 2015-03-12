9dd60c51057a926b6913f6c6e1ccab5806e23413
François Bard <francois.bard@centraliens.net>
2015-03-12 17:57:25 +0100

    first version of the models related to the board

---
+++ Diplomacy/BoardGraph.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomacy
+{
+    class BoardGraph
+    {
+        private HashSet<BoardNode> Nodes { get; set; }
+
+
+    }
+
+    class BoardNode
+    {
+        private string Name { get; set; }
+
+        private Unit Unit { get; set; } //also in coastLine.
+
+        private Force Owner { get; set; }
+
+        private SpaceType SpaceType { get; set; }
+
+        private SupplyCentre SupplyCentre { get; set; }
+
+        private HashSet<BoardNode> Neighbors { get; set; }
+
+        private HashSet<CoastLine> CoastLines { get; set; }
+    }
+
+    class CoastLine
+    {
+        private string Name { get; set; }
+
+        private Unit Unit { get; set; }
+    }
+
+    enum Unit
+    {
+        Army,
+        Fleet
+    }
+
+    class SupplyCentre
+    {
+        private Force OriginalOwner { get; set; } //needed ?
+
+        private SpaceType SpaceType { get; set; }
+    } 
+
+    enum Force
+    {
+        Russia,
+        France,
+        England,
+        Italy,
+        AustriaHungary,
+        Germany,
+        Turkey
+    }
+
+    enum SpaceType
+    {
+        Land,
+        Sea,
+        Coast
+    }
+
+}
--- Diplomacy/Program.cs
+++ Diplomacy/Program.cs
@@ -10,6 +10,8 @@ namespace Diplomacy
     {
         static void Main(string[] args)
         {
+            //build board
+            //todo
         }
     }
 }
