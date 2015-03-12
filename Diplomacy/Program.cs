074aceded5bf76490d2c5789d057be52947a8ac3
François Bard <francois.bard@centraliens.net>
2015-03-12 17:27:47 +0100

    trying to implement a diplomacy game engine
    work in progress

---
+++ Diplomacy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
