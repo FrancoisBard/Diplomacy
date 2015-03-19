6451f93a56f3fc13fe814ac33b9ebe59fc7a777d
François Bard <francois.bard@centraliens.net>
2015-03-19 17:24:29 +0100

    udpated assembly info

--- Diplomacy/Properties/AssemblyInfo.cs
+++ Diplomacy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Diplomacy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("When it's done, an order solver for diplomacy and maybe much more...")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Diplomacy")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © François Bard 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
