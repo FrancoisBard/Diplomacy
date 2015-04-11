fb58dc509380c9f4a620d2de34cb4eff5f80b0ad
francois bard <francois.bard@centraliens.net>
2015-04-11 23:15:19 +0200

    more changes to the SupplyJudge

--- Diplomacy/Orders/SupplyJudge.cs
+++ Diplomacy/Orders/SupplyJudge.cs
@@ -10,15 +10,16 @@ namespace Diplomacy.Orders
     class SupplyJudge
     {
         private Board.Board Board;
+        public IDictionary<Force, int> AdditionalUnitsNeededForEachForce; //todo make it immutable ?
 
         public SupplyJudge(Board.Board board)
         {
             Board = board;
+            AdditionalUnitsNeededForEachForce = DetermineAdditionalUnitsNeededForEachForce();
         }
 
-        public IDictionary<Force, int> DetermineAdjustments()
+        private IDictionary<Force, int> DetermineAdditionalUnitsNeededForEachForce()
         {
-
             var ownedUnits = Board.Units.
                 GroupBy(u => u.Owner);
 
@@ -36,48 +37,56 @@ namespace Diplomacy.Orders
 
         public IEnumerable<SupplyOrder> Judge(IEnumerable<SupplyOrder> orders)
         {
-            var adjustments = DetermineAdjustments();
+            //note that we don't validate the number of orders received.
+
             var executedOrders = new List<SupplyOrder>();
 
             foreach (var order in orders)
             {
-                var adjustmentsCount = adjustments[order.Force];
-
-                if (adjustmentsCount == 0)
+                if (Judge(order))
                 {
-                    //no more orders allowed
-                    continue;
+                    executedOrders.Add(order);
+                    order.Execute();
                 }
-
-                //determine if it's a disband or build order
-                var disband = adjustmentsCount < 0;
-
-                //if we disband, we need a unit. If we build, we need the location to be empty.
-                if (disband ^ order.Location.Unit == null)
-                {
-                    continue;
-                }
-
-                //check that the location is a supply centre and it's owned by the right Force
-                var province = (Province)order.Location; //todo could throw
-                if (province.SupplyCentre == null || province.SupplyCentre.Owner != order.Force)
-                {
-                    continue;
-                }
-
-                //check unit type
-                if (order.Location.SpaceType == SpaceType.Land && order.UnitType == UnitType.Fleet)
-                {
-                    continue;
-                }
-
-                //todo more ?
-
-                executedOrders.Add(order);
-                order.Execute();
             }
 
             return executedOrders;
+        }
+
+        private bool Judge(SupplyOrder order)
+        {
+            var AdditionalUnitsNeeded = AdditionalUnitsNeededForEachForce[order.Force];
+
+            //no orders to give
+            if (AdditionalUnitsNeeded == 0)
+            {
+                return false;
+            }
+
+            //determine if it's a disband or build order
+            var disband = AdditionalUnitsNeeded < 0;
+
+            //if we disband, we need a unit. If we build, we need the location to be empty.
+            if (disband ^ order.Location.Unit == null)
+            {
+                return false;
+            }
+
+            //check that the location is a supply centre and it's owned by the right Force
+            var province = (Province)order.Location; //todo could throw
+            if (province.SupplyCentre == null || province.SupplyCentre.Owner != order.Force)
+            {
+                return false;
+            }
+
+            //check unit type
+            if (order.Location.SpaceType == SpaceType.Land && order.UnitType == UnitType.Fleet)
+            {
+                return false;
+            }
+
+            //todo more ????
+            return true;
         }
     }
 }
